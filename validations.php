84fda4e1bf287a492f4329fbced40fefc28ca264
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-13 08:31:56 +0300

    create validations.php file

---
+++ validations.php
@@ -0,0 +1,14 @@
+<?php
+
+function empty_inputs($inputs)
+{
+    $data['errors'] = array();
+
+    for ($i = 0; $i < count($inputs); $i++) {
+        if ($inputs[$i] == '' || $inputs[$i] == null) {
+            $data['errors'][$i] = 'field is empty!';
+        }
+    }
+
+    return $data;
+}
